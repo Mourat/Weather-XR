6a86a23ab07f057f039a941fe0708a010016e04b
Mourat <mourat@boutry.me>
2023-02-21 11:56:47 +0100

    Solved regional problem

--- Assets/Scripts/APIManager.cs
+++ Assets/Scripts/APIManager.cs
@@ -1,20 +1,9 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 using UnityEngine.Networking;
 using Newtonsoft.Json;
-using Unity.VisualScripting;
-
-// public class WeatherData
-// {
-//     public List<string> Date { get; set; }
-//     public List<float> TemperatureMax { get; set; }
-//     public List<float> TemperatureMin { get; set; }
-//     public List<string> Sunrise { get; set; }
-//     public List<string> Sunset { get; set; }
-//     public List<string> WeatherCode { get; set; }
-// }
 
 public class APIManager : MonoBehaviour
 {
@@ -23,52 +12,31 @@ public class APIManager : MonoBehaviour
     private float _latitude;
     private float _longitude;
     private string _jsonResult;
-    public WeatherData WeatherAPIResult;
-
-    // private string _date;
-    // private string _weatherCode;
-    // private string _temperatureMax;
-    // private string _temperatureMin;
-    // private string _sunrise;
-    // private string _sunset;
+    private WeatherData _weatherAPIResult;
+    public bool hasData;
+    private NumberFormatInfo _nfi = new NumberFormatInfo();
 
     private void Awake()
     {
         _currentDate = System.DateTime.Now.ToString("yyyy-MM-dd");
         _dateAfter4Days = System.DateTime.Now.AddDays(4).ToString("yyyy-MM-dd");
-        // _latitude = 43.70f;
-        // _longitude = 7.27f;
-        // print(currentDate);
-        // print(dateAfter4Days);
+        _latitude = 43.70f;
+        _longitude = 7.27f;
+        hasData = false;
+        _nfi.NumberDecimalSeparator = ".";
     }
 
-    public void GetWeekWeather()
+    public void GetWeekWeather(float latitude, float longitude, string startDate, string endDate)
     {
-        StartCoroutine(GetWeekData());
-        // while (true)
-        // {
-        //     if (WeatherAPIResult?.GetType() != null)
-        //     {
-                foreach (var theDate in WeatherAPIResult.daily.time)
-                {
-                    print(theDate);
-                }
-        //         break;
-        //     }
-        // }
-        
+        StartCoroutine(GetWeekData(latitude, longitude, startDate, endDate));
     }
 
-    private void Start()
+    IEnumerator GetWeekData(float latitude, float longitude, string startDate, string endDate)
     {
-        GetWeekWeather();
-    }
-
-    IEnumerator GetWeekData()
-    {
+        hasData = false;
         //Replace <API_URL> with the URL of the API you want to call
         using (UnityWebRequest www = UnityWebRequest.Get(
-                   $"https://api.open-meteo.com/v1/meteofrance?latitude={_latitude}&longitude={_longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset&start_date={_currentDate}&end_date={_dateAfter4Days}&timezone=Europe%2FParis"))
+                   $"https://api.open-meteo.com/v1/meteofrance?latitude={latitude.ToString(_nfi)}&longitude={longitude.ToString(_nfi)}&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset&start_date={startDate}&end_date={endDate}&timezone=Europe%2FParis"))
         {
             yield return www.SendWebRequest();
 
@@ -81,14 +49,39 @@ public class APIManager : MonoBehaviour
                 // Get the response data as a string
                 _jsonResult = www.downloadHandler.text;
                 // Do something with the response data
-                WeatherAPIResult = JsonConvert.DeserializeObject<WeatherData>(_jsonResult);
-                foreach (var theDate in WeatherAPIResult.daily.time)
-                {
-                    print(theDate);
-                }
+                _weatherAPIResult = JsonConvert.DeserializeObject<WeatherData>(_jsonResult);
+                hasData = true;
             }
         }
+        // print(latitude);
+        // print(longitude);
+        print($"https://api.open-meteo.com/v1/meteofrance?latitude={latitude.ToString(_nfi)}&longitude={longitude.ToString(_nfi)}&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset&start_date={startDate}&end_date={endDate}&timezone=Europe%2FParis");
+
+        yield break;
     }
+
+
+    #region Remove this code
+
+    private void Start()
+    {
+        GetWeekWeather(_latitude, _longitude, _currentDate, _dateAfter4Days);
+    }
+
+    private void Update()
+    {
+        if (hasData)
+        {
+            foreach (var theDate in _weatherAPIResult.daily.time)
+            {
+                print(theDate);
+            }
+
+            hasData = false;
+        }
+    }
+
+    #endregion
 }
 
 public class WeatherData
@@ -105,6 +98,3 @@ public class Daily
     public string[] sunrise { get; set; }
     public string[] sunset { get; set; }
 }
-
-
-
